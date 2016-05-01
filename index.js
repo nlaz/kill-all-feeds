897aeac195ba3d39e5b0f85d2473f7ecd1a4d1b4
nlaz <nikolazaris@utexas.edu>
2016-04-30 22:37:41 -0500

    kill youtube feeds

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var killFeeds = function () {
-    var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
 
-    feed.children().remove();
-    $('.cardRightCol').remove();
+  /* Facebook Feed */
+  var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
+  feed.children().remove();
+  $('.cardRightCol').remove();
+
+  /* Youtube Feed */
+  $('#feed').remove();
+  $('.appbar-nav-menu').remove();
+  $('#guide-container').remove();
+  $('#autoplay-checkbox').attr('checked', false);
+  $('#watch7-sidebar-contents').remove();
 }
 
 window.setInterval(killFeeds, 100);
