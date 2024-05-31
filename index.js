0750df47051dbf7badbf3108254d896f35ab6e83
Niko Lazaris <niko@thegarage.dev>
2024-05-31 15:30:47 -0400

    update

--- index.js
+++ index.js
@@ -7,24 +7,26 @@ var killFeeds = function () {
   }
 
   if (window.location.pathname === "/") {
-    $("#page-manager").remove();
-    $("#home-container-media").remove();
-    $("#guide-content").remove();
+    var pageManager = document.getElementById("page-manager");
+    if (pageManager) pageManager.remove();
+
+    var homeContainerMedia = document.getElementById("home-container-media");
+    if (homeContainerMedia) homeContainerMedia.remove();
+
+    var guideContent = document.getElementById("guide-content");
+    if (guideContent) guideContent.remove();
   }
-  $("#related").remove();
+
+  var related = document.getElementById("related");
+  if (related) related.remove();
+
   /* Hide Explore Feed */
-  $(".ytd-page-manager[page-subtype='explore']").remove();
-  $(".ytd-page-manager[page-subtype='subscriptions']").remove();
-  $(".ytd-mini-guide-renderer[aria-label='Explore']").remove();
-  $(".ytd-mini-guide-renderer[aria-label='Shorts']").remove();
-  $(".ytd-guide-entry-renderer[title='Explore']").remove();
-  $(".ytd-guide-entry-renderer[title='Shorts']").remove();
-
-  /* HN Feed */
-  $(".itemlist").remove();
-
-  /* Reddit Feed */
-  $("#2x-container").remove();
+  var exploreFeeds = document.querySelectorAll(
+    ".ytd-page-manager[page-subtype='explore'], .ytd-page-manager[page-subtype='subscriptions'], .ytd-mini-guide-renderer[aria-label='Explore'], .ytd-mini-guide-renderer[aria-label='Shorts'], .ytd-guide-entry-renderer[title='Explore'], .ytd-guide-entry-renderer[title='Shorts']"
+  );
+  exploreFeeds.forEach(function (element) {
+    element.remove();
+  });
 };
 
-window.setInterval(killFeeds, 200);
+window.setInterval(killFeeds, 150);
